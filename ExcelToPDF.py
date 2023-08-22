079c0c2c05c33d27dfe63ee26ef9a958a36e483d
Benjamin Contreras <91103275+Benjax14@users.noreply.github.com>
2023-08-22 04:01:57 -0400

    v2

--- ExcelToPDF.py
+++ ExcelToPDF.py
@@ -1,12 +1,23 @@
 import pandas as pd
+import locale
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
+from datetime import datetime
+
+# Configura el locale para español
+locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 
 # Lee el archivo Excel
 excel_file = "ramos.xlsx"
 df = pd.read_excel(excel_file)
+
+# Obtiene la fecha actual
+actualDate = datetime.now()
+
+# Formatea la fecha actual en el formato deseado (por ejemplo, "día de mes de año")
+fixedDate = actualDate.strftime("%d de %B de %Y")
 
 # Separa los datos por "ramos"
 ramos = df['Seleccione ramo/s'].str.split(', ').explode().unique()
@@ -15,6 +26,21 @@ ramos = df['Seleccione ramo/s'].str.split(', ').explode().unique()
 pdf_file = "resultado.pdf"
 doc = SimpleDocTemplate(pdf_file, pagesize=letter)
 elements = []
+
+cover_text = """
+<size=30><b>Resultados de Ramos no Impartidos</b></size>
+"""
+
+cover_date = f"""
+<size=20>Fecha: {fixedDate}</size>
+"""
+
+# Agrega una portada al principio del documento
+cover_title_paragraph = Paragraph(cover_text, getSampleStyleSheet()['Title'])
+cover_date_paragraph = Paragraph(cover_date, getSampleStyleSheet()['Title'])
+elements.append(cover_title_paragraph)
+elements.append(cover_date_paragraph)
+elements.append(PageBreak())  # Agregar un salto de página después de la portada
 
 # Crea una tabla separada para cada "ramo"
 for ramo in ramos:
